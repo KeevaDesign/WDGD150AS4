ab17eccf744c3b52ade55dbcc667f22fd7d35302
keeva <keevang.design@gmail.com>
2024-04-06 12:24:16 -0400

    edit api

--- js/weather.js
+++ js/weather.js
@@ -1,17 +1,11 @@
-const apiKey = "07d67f84cd34e2452408a52a5a0c1414";
+require("dotenv").config();
+const apiKey = process.env.API_KEY;
 const baseURL = "https://api.openweathermap.org/data/2.5/weather?";
 
 const cityName = "Toronto";
 const countryCode = "CA";
 
 var today = new Date().toDateString(); // get today's Date
-
-// https://api.openweathermap.org/data/2.5/weather?q=Toronto,canada&appid=07d67f84cd34e2452408a52a5a0c1414
-
-// Current Weather Conditions
-// Location
-// Icon (this is an image, look into the API documentation for details)
-// Temperature (in celsius, look into the API documentation for details)
 
 function getWeatherData() {
   const url = `${baseURL}q=${cityName},${countryCode}&appid=${apiKey}&units=metric`;
